f819a2f24acea57ca63655274285d18b7c758b89
Jeremy Audet <ichimonji10@gmail.com>
2014-12-16 09:28:27 -0500

    Copy robottelo.api.client to nailgun.client
    Make some minor changes to the module-wide docstring while copying over code,
    but otherwise, leave the code exactly as it is in Robottelo commit
    0f0b374b620025a619e020b921aaee33a536aad0.

---
+++ nailgun/__init__.py
@@ -0,0 +1,9 @@
+"""The root of the NailGun namespace.
+
+NailGun's modules are organized in to layers, from bottom to top, like so:
+
+1. :mod:`nailgun.client`
+2. :mod:`nailgun.orm`
+3. :mod:`nailgun.entities`
+
+"""
