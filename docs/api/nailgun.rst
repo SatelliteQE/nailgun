952336683e0f5910d0a72bfc4c7a3761947a0e30
Jeremy Audet <ichimonji10@gmail.com>
2015-01-08 10:21:19 -0500

    Create class `BaseServerConfig`, polish docstrings
    Make class `ServerConfig` inherit from `BaseServerConfig`. Do this because class
    `BaseServerConfig` is broadly useful and should be available to other clients.
    The public API of class `ServerConfig` is unchanged.
    
    Make several other changes that help facilitate these changes.
    
    * Drop the `_SENTINEL` object. The `url` and `auth` arguments to `ServerConfig`
      were the only users of the object, `None` should be a suitable default value
      to the arguments, and it is easier to subclass when `None` is used as a
      default argument.
    * Drop the module-level constants `XDG_CONFIG_DIR` and `XDG_CONFIG_FILE` and add
      the class attributes `_xdg_config_dir` and `_xdg_config_file` to class
      `BaseServerConfig`. This makes the class more self-contained and allows for
      easier subclassing.
    * Add `xdg_config_dir` and `xdg_config_file` arguments to function
      `_get_config_file_path`, and drop its reliance on the module-level constants
      `XDG_CONFIG_DIR` and `XDG_CONFIG_FILE`.
    
    Also:
    
    * Polish docstrings.
    * Update the unit test suite.
    * Include private members in the API documentation for module `nailgun.config`.
      This is because the `_get_config_file_path` function is integral to the
      functioning of the module, and including it in the API documentation reduces
      duplication of information.

--- docs/api/nailgun.rst
+++ docs/api/nailgun.rst
@@ -14,3 +14,4 @@
 
 .. automodule:: nailgun.config
    :members:
+   :private-members:
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,6 @@
 """Unit tests for :mod:`nailgun.config`."""
 from mock import call, mock_open, patch
-from nailgun.config import ServerConfig
+from nailgun.config import BaseServerConfig, ServerConfig
 from unittest import TestCase
 import json
 
@@ -18,57 +18,47 @@ FILE_PATH = '/tmp/bogus.json'
 CONFIGS = {
     'default': {'url': 'http://example.com'},
     'Ask Aak': {'url': 'bogus value', 'auth': ['username', 'password']},
+}
+CONFIGS2 = CONFIGS.copy()
+CONFIGS2.update({
     'Abeloth': {'url': 'bogus value', 'verify': True},
     'Admiral Gial Ackbar': {'url': 'bogus', 'auth': [], 'verify': False},
-}
+})
+# The pylint star-args warning is disabled for several tests. This is because
+# star args make the relevant tests _so_ much more compact than any
+# alternatives, and the dicts in question are hardcoded right here.
 
 
-class ServerConfigTestCase(TestCase):
-    """Tests for :class:`nailgun.config.ServerConfig`."""
-    # The pylint star-args warning is disabled for several tests. This is
-    # because star args make the relevant tests _so_ much more compact than any
-    # alternatives, and the dicts in question are hardcoded in this module.
+class BaseServerConfigTestCase(TestCase):
+    """Tests for :class:`nailgun.config.BaseServerConfig`."""
 
     def test_init(self):
-        """Test instantiating :class:`nailgun.config.ServerConfig`.
+        """Test instantiating :class:`nailgun.config.BaseServerConfig`.
 
         Assert that only provided values become object attributes.
 
         """
-        for _, config in CONFIGS.items():
+        for config in CONFIGS.values():
             # pylint:disable=star-args
-            self.assertEqual(config, vars(ServerConfig(**config)))
-
-    def test_get_client_kwargs(self):
-        """Test :meth:`nailgun.config.ServerConfig.get_client_kwargs`.
-
-        Assert that all attributes passed in are returned, but with "url"
-        omitted.
-
-        """
-        for _, config in CONFIGS.items():
-            out = config.copy()
-            out.pop('url')
-            # pylint:disable=star-args
-            self.assertEqual(out, ServerConfig(**config).get_client_kwargs())
+            self.assertEqual(config, vars(BaseServerConfig(**config)))
 
     def test_get(self):
-        """Test :meth:`nailgun.config.ServerConfig.get`.
+        """Test :meth:`nailgun.config.BaseServerConfig.get`.
 
         Assert that the method extracts the asked-for section from a
-        configuration file and correctly populates a new ``ServerConfig``
+        configuration file and correctly populates a new ``BaseServerConfig``
         object.
 
         """
         for label, config in CONFIGS.items():
             open_ = mock_open(read_data=json.dumps(CONFIGS))
             with patch.object(builtins, 'open', open_):
-                server_config = ServerConfig.get(label, FILE_PATH)
+                server_config = BaseServerConfig.get(label, FILE_PATH)
             self.assertEqual(vars(server_config), config)
             open_.assert_called_once_with(FILE_PATH)
 
     def test_get_labels(self):
-        """Test :meth:`nailgun.config.ServerConfig.get_labels`.
+        """Test :meth:`nailgun.config.BaseServerConfig.get_labels`.
 
         Assert that the method returns the correct labels.
 
@@ -77,12 +67,12 @@ class ServerConfigTestCase(TestCase):
         with patch.object(builtins, 'open', open_):
             self.assertEqual(
                 set(CONFIGS.keys()),
-                set(ServerConfig.get_labels(FILE_PATH)),
+                set(BaseServerConfig.get_labels(FILE_PATH)),
             )
         open_.assert_called_once_with(FILE_PATH)
 
     def test_save(self):
-        """Test :meth:`nailgun.config.ServerConfig.save`.
+        """Test :meth:`nailgun.config.BaseServerConfig.save`.
 
         Assert that the method reads the config file before writing, and that
         it writes out a correct config file.
@@ -92,7 +82,7 @@ class ServerConfigTestCase(TestCase):
         config = {label: {'url': 'https://example.org'}}
         open_ = mock_open(read_data=json.dumps(CONFIGS))
         with patch.object(builtins, 'open', open_):
-            ServerConfig(config[label]['url']).save(label, FILE_PATH)
+            BaseServerConfig(config[label]['url']).save(label, FILE_PATH)
 
         # We care about two things: that this method reads the config file
         # before writing, and that the written string is correct. The first is
@@ -108,7 +98,7 @@ class ServerConfigTestCase(TestCase):
         self.assertEqual(target_config, actual_config)
 
     def test_delete(self):
-        """Test :meth:`nailgun.config.ServerConfig.delete`.
+        """Test :meth:`nailgun.config.BaseServerConfig.delete`.
 
         Assert that the method reads the config file before writing, and that
         it writes out a correct config file.
@@ -116,7 +106,7 @@ class ServerConfigTestCase(TestCase):
         """
         open_ = mock_open(read_data=json.dumps(CONFIGS))
         with patch.object(builtins, 'open', open_):
-            ServerConfig.delete('Ask Aak', FILE_PATH)
+            BaseServerConfig.delete('Ask Aak', FILE_PATH)
 
         # See `test_save` for further commentary on what's being done here.
         self.assertEqual(
@@ -127,6 +117,33 @@ class ServerConfigTestCase(TestCase):
         target_config = CONFIGS.copy()
         del target_config['Ask Aak']
         self.assertEqual(target_config, actual_config)
+
+
+class ServerConfigTestCase(TestCase):
+    """Tests for :class:`nailgun.config.ServerConfig`."""
+
+    def test_init(self):
+        """Test instantiating :class:`nailgun.config.ServerConfig`.
+
+        Assert that only provided values become object attributes.
+
+        """
+        for config in CONFIGS2.values():
+            # pylint:disable=star-args
+            self.assertEqual(config, vars(ServerConfig(**config)))
+
+    def test_get_client_kwargs(self):
+        """Test :meth:`nailgun.config.ServerConfig.get_client_kwargs`.
+
+        Assert that all attributes passed in are returned, but with "url"
+        omitted.
+
+        """
+        for config in CONFIGS2.values():
+            out = config.copy()
+            out.pop('url')
+            # pylint:disable=star-args
+            self.assertEqual(out, ServerConfig(**config).get_client_kwargs())
 
 
 def _get_written_json(mock_obj):
