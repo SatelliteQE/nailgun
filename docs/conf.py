56840da637cff96a71a263f3a676f86ca7fb3ee4
Jeremy Audet <ichimonji10@gmail.com>
2015-04-17 17:01:01 -0400

    Release version 0.9.0
    Changes in this release:
    
    * Fix several bugs. Drop a redundant assignment to
      `nailgun.entity_fields.ENTITY_MODULES` and add unit tests for the
      `create_payload` methods.
    * Add method `ActivationKey.content_override`.

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ sys.path.insert(
 
 project = 'NailGun'
 copyright = '2014, Jeremy Audet'  # pylint:disable=redefined-builtin
-version = '0.8.1'
+version = '0.9.0'
 release = version
 
 # General Configuration -------------------------------------------------------
