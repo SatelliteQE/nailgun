SatelliteQE/nailgun

commit c420ab6e1ecbe2b7b802a8b9114bb1a87f2f8529

Update HostGroup entity
Add the `content_view` and `lifecycle_environment` fields to the `HostGroup`
entity. Both fields are covered by the API documentation, but neither field is
returned by an HTTP GET request to `/api/v2/hostgroups/:id`. In response, file
two bugs and add a workaround to the `read` method.

Add `EntityUpdateMixin` to the `HostGroup` entity. Add an `update` method that
works around two more bugs.

Update the test suite to fix tests that break as a result of these changes.

nailgun/entities.py
@@ -1519,7 +1519,11 @@ class HostCollection(
 
 
 class HostGroup(
-        Entity, EntityCreateMixin, EntityDeleteMixin, EntityReadMixin):
+        Entity,
+        EntityCreateMixin,
+        EntityDeleteMixin,
+        EntityReadMixin,
+        EntityUpdateMixin):
     """A representation of a Host Group entity."""
 
     def __init__(self, server_config=None, **kwargs):
@@ -1528,8 +1532,16 @@ class HostGroup(
                 Architecture,
                 null=True,
             ),
+            'content_view': entity_fields.OneToOneField(
+                ContentView,
+                null=True,
+            ),
             'domain': entity_fields.OneToOneField(Domain, null=True),
             'environment': entity_fields.OneToOneField(Environment, null=True),
+            'lifecycle_environment': entity_fields.OneToOneField(
+                LifecycleEnvironment,
+                null=True,
+            ),
             'location': entity_fields.OneToManyField(Location, null=True),
             'medium': entity_fields.OneToOneField(Media, null=True),
             'name': entity_fields.StringField(required=True),
@@ -1559,11 +1571,14 @@ class HostGroup(
         return {u'hostgroup': super(HostGroup, self).create_payload()}
 
     def read(self, entity=None, attrs=None, ignore=()):
-        """Deal with weirdly named data returned from the server.
+        """Deal with several bugs.
 
-        When creating a HostGroup, the server accepts a field named 'parent'.
-        When reading a HostGroup, the server returns a semantically identical
-        field named 'ancestry'.
+        For more information, see:
+
+        * `Bugzilla #1235377
+          <https://bugzilla.redhat.com/show_bug.cgi?id=1235377>`_
+        * `Bugzilla #1235379
+          <https://bugzilla.redhat.com/show_bug.cgi?id=1235379>`_
 
         """
         if attrs is None:
@@ -1573,8 +1588,28 @@ class HostGroup(
             attrs['parent'] = None
         else:
             attrs['parent'] = {'id': parent_id}
-
+        update_attrs = self.update_json([])
+        for attr in ('content_view_id', 'lifecycle_environment_id'):
+            attrs[attr] = update_attrs[attr]
         return super(HostGroup, self).read(entity, attrs, ignore)
+
+    def update(self, fields=None):
+        """Deal with several bugs.
+
+        For more information, see:
+
+        * `Bugzilla #1235378
+          <https://bugzilla.redhat.com/show_bug.cgi?id=1235378>`_
+        * `Bugzilla #1235380
+          <https://bugzilla.redhat.com/show_bug.cgi?id=1235380>`_
+
+        """
+        self.update_json(fields)
+        return self.read()
+
+    def update_payload(self, fields=None):
+        """Wrap submitted data within an extra dict."""
+        return {u'hostgroup': super(HostGroup, self).update_payload(fields)}
 
 
 class Host(  # pylint:disable=too-many-instance-attributes

tests/test_entities.py
@@ -713,10 +713,10 @@ class ReadTestCase(TestCase):
         """
         for entity in (
                 # entities.DockerComputeResource,  # see test_attrs_arg_v2
+                # entities.HostGroup,  # see HostGroupTestCase.test_read
                 # entities.UserGroup,  # see test_attrs_arg_v2
                 entities.Domain,
                 entities.Host,
-                entities.HostGroup,
                 entities.Media,
                 entities.Product,
                 entities.RHCIDeployment,
@@ -730,10 +730,10 @@ class ReadTestCase(TestCase):
                         self.assertEqual(read.call_count, 1)
 
     def test_attrs_arg_v2(self):
-        """Validate :meth:`nailgun.entities.UserGroup.read`.
+        """Ensure ``read``, ``read_json`` and ``client.put`` are called once.
 
-        Check that the method calls ``read`` and ``read_json`` once, and that
-        ``client.put`` is used to read the ``'admin'`` attribute.
+        This test is only appropriate for entities that override the ``read``
+        method in order to fiddle with the ``attrs`` argument.
 
         """
         # test_data is a single-use variable. We use it anyway for formatting
@@ -778,11 +778,6 @@ class ReadTestCase(TestCase):
                 entities.Host(self.cfg),
                 {'parameters': None, 'puppetclasses': None},
                 {'host_parameters_attributes': None, 'puppet_class': None},
-            ),
-            (
-                entities.HostGroup(self.cfg),
-                {'ancestry': None},
-                {'parent': None},
             ),
             (
                 entities.System(self.cfg),
@@ -847,6 +842,7 @@ class UpdateTestCase(TestCase):
             entities.ConfigTemplate(self.cfg),
             entities.Domain(self.cfg),
             entities.Host(self.cfg),
+            entities.HostGroup(self.cfg),
             entities.Organization(self.cfg),
             entities.User(self.cfg),
         )
@@ -892,6 +888,7 @@ class UpdatePayloadTestCase(TestCase):
             (entities.ConfigTemplate, {'config_template': {}}),
             (entities.Domain, {'domain': {}}),
             (entities.Host, {'host': {}}),
+            (entities.HostGroup, {'hostgroup': {}}),
             (entities.Organization, {'organization': {}}),
             (entities.User, {'user': {}}),
         ]
@@ -1050,6 +1047,38 @@ class ActivationKeyTestCase(TestCase):
         self.assertEqual(
             client_put.call_args[0][1]['content_override'],
             {'content_label': content_label, 'value': value},
+        )
+
+
+class HostGroupTestCase(TestCase):
+    """Tests for :class:`nailgun.entities.HostGroup`."""
+
+    def test_read(self):
+        """Ensure ``read``, ``read_json`` and ``update_json`` are called once.
+
+        This test is only appropriate for entities that override the ``read``
+        method in order to fiddle with the ``attrs`` argument.
+
+        """
+        entity = entities.HostGroup(config.ServerConfig('some url'))
+        with mock.patch.object(entity, 'read_json') as read_json:
+            read_json.return_value = {'ancestry': None}
+            with mock.patch.object(entity, 'update_json') as update_json:
+                update_json.return_value = {
+                    'content_view_id': None,
+                    'lifecycle_environment_id': None,
+                }
+                with mock.patch.object(EntityReadMixin, 'read') as read:
+                    entity.read()
+        for meth in (read_json, update_json, read):
+            self.assertEqual(meth.call_count, 1)
+        self.assertEqual(
+            read.call_args[0][1],
+            {
+                'content_view_id': None,
+                'lifecycle_environment_id': None,
+                'parent': None,
+            },
         )
 
 

commit 2695bb30a68abe8ac62379374fa8b2142adaabb9

Fix bug in method `HostGroup.read`
A bug was introduced in c420ab6e1ecbe2b7b802a8b9114bb1a87f2f8529:

    >>> from nailgun import entities
    >>> host_group = entities.HostGroup(parent=1).create(True)
    Traceback (most recent call last):
    File "<stdin>", line 1, in <module>
    File "/home/ichimonji10/code/nailgun/nailgun/entity_mixins.py", line 837, in create
        return self.read(attrs=self.create_json(create_missing))
    File "/home/ichimonji10/code/nailgun/nailgun/entities.py", line 1591, in read
        update_attrs = self.update_json([])
    File "/home/ichimonji10/code/nailgun/nailgun/entity_mixins.py", line 909, in update_json
        response = self.update_raw(fields)
    File "/home/ichimonji10/code/nailgun/nailgun/entity_mixins.py", line 890, in update_raw
        self.path('self'),
    File "/home/ichimonji10/code/nailgun/nailgun/entity_mixins.py", line 471, in path
        raise NoSuchPathError
    nailgun.entity_mixins.NoSuchPathError

Manual testing shows that this fix works:

    >>> from nailgun import entities
    >>> host_group = entities.HostGroup(parent=1).create(True)
    >>> host_group.id
    34
    >>> host_group.parent.id
    u'1'

Normal usages of `HostGroup.read` still work fine:

    >>> from nailgun import entities
    >>> entities.HostGroup(id=34).read().parent.id
    '1'
    >>> print(entities.HostGroup(id=1).read().parent)
    None

Robottelo is happy too:

    $ nosetests tests/foreman/api/test_multiple_paths.py -m HostGroup
    .........SS
    ----------------------------------------------------------------------
    Ran 11 tests in 11.700s

    OK (SKIP=2)

nailgun/entities.py
@@ -1588,9 +1588,11 @@ class HostGroup(
             attrs['parent'] = None
         else:
             attrs['parent'] = {'id': parent_id}
-        update_attrs = self.update_json([])
+        # We cannot call `self.update_json([])`, as an ID might not be present
+        # on self. However, `attrs` is guaranteed to have an ID.
+        attrs2 = HostGroup(self._server_config, id=attrs['id']).update_json([])
         for attr in ('content_view_id', 'lifecycle_environment_id'):
-            attrs[attr] = update_attrs[attr]
+            attrs[attr] = attrs2[attr]
         return super(HostGroup, self).read(entity, attrs, ignore)
 
     def update(self, fields=None):

tests/test_entities.py
@@ -1065,12 +1065,15 @@ class HostGroupTestCase(TestCase):
         """
         entity = entities.HostGroup(config.ServerConfig('some url'))
         with mock.patch.object(entity, 'read_json') as read_json:
-            read_json.return_value = {'ancestry': None}
-            with mock.patch.object(entity, 'update_json') as update_json:
-                update_json.return_value = {
+            read_json.return_value = {'ancestry': None, 'id': 641212}  # random
+            with mock.patch.object(
+                entities.HostGroup,
+                'update_json',
+                return_value={
                     'content_view_id': None,
                     'lifecycle_environment_id': None,
-                }
+                },
+            ) as update_json:
                 with mock.patch.object(EntityReadMixin, 'read') as read:
                     entity.read()
         for meth in (read_json, update_json, read):
@@ -1079,6 +1082,7 @@ class HostGroupTestCase(TestCase):
             read.call_args[0][1],
             {
                 'content_view_id': None,
+                'id': 641212,
                 'lifecycle_environment_id': None,
                 'parent': None,
             },