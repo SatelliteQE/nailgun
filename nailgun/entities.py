94413be891653e858914267c9a8a27b6baadc8b4
Jeremy Audet <ichimonji10@gmail.com>
2015-09-01 13:07:51 -0400

    Make content view filter rules searchable
    Sample test results:
    
        >>> from nailgun import entities
        >>> cv_filter = entities.RPMContentViewFilter(content_view=1).create(True)
        >>> entities.ContentViewFilterRule(content_view_filter=cv_filter).search()
        []

--- nailgun/entities.py
+++ nailgun/entities.py
@@ -1120,7 +1120,11 @@ class ContentViewVersion(Entity, EntityReadMixin, EntityDeleteMixin):
 
 
 class ContentViewFilterRule(
-        Entity, EntityCreateMixin, EntityDeleteMixin, EntityReadMixin):
+        Entity,
+        EntityCreateMixin,
+        EntityDeleteMixin,
+        EntityReadMixin,
+        EntitySearchMixin):
     """A representation of a Content View Filter Rule entity."""
 
     def __init__(self, server_config=None, **kwargs):
