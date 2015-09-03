d12bb20bd575e692a293a635ff3cdaa0e526b854
sghai <sghai@redhat.com>
2015-09-03 12:56:55 +0530

    Updated 'subnet' entity with search and update mixin

--- nailgun/entities.py
+++ nailgun/entities.py
@@ -3336,7 +3336,12 @@ class Status(Entity):
 
 
 class Subnet(
-        Entity, EntityCreateMixin, EntityDeleteMixin, EntityReadMixin):
+        Entity,
+        EntityCreateMixin,
+        EntityDeleteMixin,
+        EntityReadMixin,
+        EntitySearchMixin,
+        EntityUpdateMixin):
     """A representation of a Subnet entity."""
 
     def __init__(self, server_config=None, **kwargs):
@@ -3396,6 +3401,10 @@ class Subnet(
             ignore = set()
         ignore.add('discovery')
         return super(Subnet, self).read(entity, attrs, ignore)
+
+    def update_payload(self, fields=None):
+        """Wrap submitted data within an extra dict."""
+        return {u'subnet': super(Subnet, self).update_payload(fields)}
 
 
 class Subscription(
