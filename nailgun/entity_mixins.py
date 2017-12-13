749fcaa72b2ea0f031daa7c61154d428a7e69e21
Sean O'Keeffe <seanokeeffe797@gmail.com>
2017-12-13 14:22:02 +0000

    handle raise MissingValueError if we cannt find ID
    This is needed as other code handles MissingValueError but doesnt handle KeyError.

--- nailgun/entity_mixins.py
+++ nailgun/entity_mixins.py
@@ -242,19 +242,23 @@ def _get_entity_id(field_name, attrs):
 
     """
     field_name_id = field_name + '_id'
-    if field_name in attrs:
-        if attrs[field_name] is None:
-            return None
+
+    error_msg = """ Cannot find a value for the "{0}" field. Searched
+                    for keys named {1}, but available keys are {2}.
+                """.format(field_name, (field_name, field_name_id),
+                           attrs.keys())
+    try:
+        if field_name in attrs:
+            if attrs[field_name] is None:
+                return None
+            elif 'id' in attrs[field_name]:
+                return attrs[field_name]['id']
+        if field_name_id in attrs:
+            return attrs[field_name_id]
         else:
-            return attrs[field_name]['id']
-    elif field_name_id in attrs:
-        return attrs[field_name_id]
-    else:
-        raise MissingValueError(
-            'Cannot find a value for the "{0}" field. Searched for keys named '
-            '{1}, but available keys are {2}.'
-            .format(field_name, (field_name, field_name_id), attrs.keys())
-        )
+            raise MissingValueError(error_msg)
+    except KeyError:
+        raise MissingValueError(error_msg)
 
 
 def _get_entity_ids(field_name, attrs):
