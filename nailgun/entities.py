e4bb112ec6b37012f3cafed2d14db89b80924da1
Mirosław Zalewski <mzalewsk@redhat.com>
2018-11-29 13:25:12 +0100

    Add registry name fields to Lifecycle Environment

--- nailgun/entities.py
+++ nailgun/entities.py
@@ -4062,6 +4062,8 @@ class LifecycleEnvironment(
                 required=True,
             ),
             'prior': entity_fields.OneToOneField(LifecycleEnvironment),
+            'registry_name_pattern': entity_fields.StringField(),
+            'registry_unauthenticated_pull': entity_fields.BooleanField(),
         }
         self._meta = {
             'api_path': 'katello/api/v2/environments',
