6aa3e21e1a0690be3100829317f1570a15a6e4cc
Djebran Lezzoum <ldjebran@gmail.com>
2019-01-02 08:22:38 +0200

    Add Host packages (#597)
    Added Support for entities.Host.packages()

--- nailgun/entities.py
+++ nailgun/entities.py
@@ -3828,6 +3828,22 @@ class Host(  # pylint:disable=too-many-instance-attributes,R0904
         response = client.get(self.path('errata'), **kwargs)
         return _handle_response(response, self._server_config, synchronous)
 
+    def packages(self, synchronous=True, **kwargs):
+        """List packages installed on the host
+
+        :param synchronous: What should happen if the server returns an HTTP
+            202 (accepted) status code? Wait for the task to complete if
+            ``True``. Immediately return the server's response otherwise.
+        :param kwargs: Arguments to pass to requests.
+        :returns: The server's response, with all content decoded.
+        :raises: ``requests.exceptions.HTTPError`` If the server responds with
+            an HTTP 4XX or 5XX message.
+        """
+        kwargs = kwargs.copy()  # shadow the passed-in kwargs
+        kwargs.update(self._server_config.get_client_kwargs())
+        response = client.get(self.path('packages'), **kwargs)
+        return _handle_response(response, self._server_config, synchronous)
+
     def module_streams(self, synchronous=True, **kwargs):
         """List module_streams available for the host
 
@@ -4033,6 +4049,7 @@ class Host(  # pylint:disable=too-many-instance-attributes,R0904
                 'errata/apply',
                 'errata/applicability',
                 'facts',
+                'packages',
                 'power',
                 'puppetclass_ids',
                 'smart_class_parameters',
