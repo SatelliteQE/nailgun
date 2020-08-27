47d749df5b5c55548bfcf5c3f6b26cdab5aab50c
Peter Ondrejka <pondrejk@redhat.com>
2020-08-27 13:14:04 +0200

    restore permissions resource type

--- nailgun/entities.py
+++ nailgun/entities.py
@@ -5381,6 +5381,7 @@ class Permission(Entity, EntityReadMixin, EntitySearchMixin):
                 length=(6, 12),
                 unique=True
             ),
+            'resource_type': entity_fields.StringField(required=True),
         }
         self._meta = {
             'api_path': 'api/v2/permissions',
