0a76dd1c7599439040b96273237e8016c80704ed
jyejare <jyejare@redhat.com>
2022-03-02 18:20:33 +0530

    More py36 removal and python base version in setup

--- setup.py
+++ setup.py
@@ -42,10 +42,10 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.10',
     ],
     packages=find_packages(exclude=['docs', 'tests']),
     install_requires=REQUIREMENTS,
+    python_requires='>=3.8',
 )
