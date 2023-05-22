9d5b535c18762aa15e111c503d2d691652093f14
David Moore <109112035+damoore044@users.noreply.github.com>
2023-05-22 11:05:25 -0400

    Add cacert to .fields in HttpProxy (#923)
    * Add cacert to .fields in HttpProxy
    
    * ignore cacert in http-proxy.read()

--- nailgun/entities.py
+++ nailgun/entities.py
@@ -5006,6 +5006,7 @@ class HTTPProxy(
             'password': entity_fields.StringField(),
             'organization': entity_fields.OneToManyField(Organization),
             'location': entity_fields.OneToManyField(Location),
+            'cacert': entity_fields.StringField(),
         }
         self._meta = {'api_path': 'api/v2/http_proxies'}
         super().__init__(server_config, **kwargs)
@@ -5031,6 +5032,7 @@ class HTTPProxy(
         ignore.add('password')
         ignore.add('organization')
         ignore.add('location')
+        ignore.add('cacert')
         return super().read(entity, attrs, ignore, params)
 
 
