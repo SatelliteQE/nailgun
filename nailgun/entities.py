fd48ffefb8a34da28150cc43d2486b7bc38cebc4
Adarsh dubey <addubey@redhat.com>
2023-07-11 15:52:31 +0530

    Adding entity mixins classes (#954)
    Signed-off-by: Adarsh Dubey <addubey@redhat.com>

--- nailgun/entities.py
+++ nailgun/entities.py
@@ -8575,7 +8575,14 @@ class AnsiblePlaybooks(Entity):
         return _handle_response(response, self._server_config, synchronous, timeout)
 
 
-class AnsibleRoles(Entity):
+class AnsibleRoles(
+    Entity,
+    EntityCreateMixin,
+    EntityDeleteMixin,
+    EntityReadMixin,
+    EntitySearchMixin,
+    EntityUpdateMixin,
+):
     """A representation of Ansible Roles entity."""
 
     def __init__(self, server_config=None, **kwargs):
