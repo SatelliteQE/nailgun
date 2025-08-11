3da18695294a9ebb0e81956beab0e90413e92ba2
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2025-08-11 05:44:21 -0400

    Add description key to _fields dictionary (#1324) (#1325)
    Co-authored-by: Nakul Pathak <33572967+nacoool@users.noreply.github.com>
    Co-authored-by: Nakul Pathak <napathak@napathak-thinkpadt14gen5.punetw6.csb>

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -2860,6 +2860,7 @@ class JobTemplateTestCase(TestCase):
         self.data = {
             'id': 1,
             'audit_comment': None,
+            'description': None,
             'description_format': None,
             'effective_user': None,
             'job_category': 'Commands',
